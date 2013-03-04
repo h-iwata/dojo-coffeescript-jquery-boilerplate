1f9b9853c7c02b8538c96ba4e49eb6985c72a0c9
h-iwata <h.iwata@astamuse.co.jp>
2013-03-04 16:02:56 +0900

    startCoffeeWatch.sh is now available at any corrent directories

--- resources/js/tools/startCoffeeWatch.sh
+++ resources/js/tools/startCoffeeWatch.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-APP_NAME="App"
-coffee -cw --output ../src/$APP_NAME ../coffee/$APP_NAME
+APP_NAME="jobofferSourcing"
+BASEDIR=$(cd $(dirname $0) && pwd)
+coffee -cw --output $BASEDIR/../src/$APP_NAME $BASEDIR/../coffee/$APP_NAME
