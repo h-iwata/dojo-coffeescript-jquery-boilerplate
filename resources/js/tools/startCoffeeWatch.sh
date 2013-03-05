df5f0e349d93ea2a1695b496005f1b1bcce98854
h-iwata <h.iwata@astamuse.co.jp>
2013-03-05 14:34:15 +0900

    remove appname

--- resources/js/tools/startCoffeeWatch.sh
+++ resources/js/tools/startCoffeeWatch.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-APP_NAME="App"
 BASEDIR=$(cd $(dirname $0) && pwd)
-coffee -cw --output $BASEDIR/../src/$APP_NAME $BASEDIR/../coffee/$APP_NAME
+coffee -cw --output $BASEDIR/../src $BASEDIR/../coffee
