5b3933f257cda9aa39c978906087b11c95a65ad1
h-iwata <h.iwata@astamuse.co.jp>
2013-03-21 17:23:16 +0900

    remove profile.js

--- tools/build/profile.js
+++ tools/build/profile.js
@@ -19,4 +19,16 @@ var profile = {
 	layers: {
 		'App/Controllers/Index' :{},
 	},
+	
+	resourceTags:{
+		test: function (filename, mid) {
+			return false;
+		},
+		copyOnly: function (filename, mid) {
+			return (/^\/resources\/js\/lib\//.test(filename));
+		},
+		amd: function(filename, mid){
+			return !this.copyOnly(filename, mid) && /\.js$/.test(filename);
+		}
+	}
 };
