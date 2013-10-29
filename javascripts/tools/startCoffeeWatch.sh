3e0b907c06e4db473ef5d2d5420a5253cf3f33cb
h-iwata <h.iwata@astamuse.co.jp>
2013-10-29 16:42:35 +0900

    clone dojo when its not exists

--- javascripts/tools/startCoffeeWatch.sh
+++ javascripts/tools/startCoffeeWatch.sh
@@ -1,6 +1,32 @@
 #!/bin/sh
+
 BASEDIR=$(cd $(dirname $0) && pwd)
+
+# Source directory for unbuilt code
+SRCDIR="$BASEDIR/../../src"
+
 echo 'set debug mode to config.js'
 perl -pwi -e "s/var debug = false;/var debug = true;/g" "$BASEDIR/../config.js"
 echo 'done!'
+
+if [ ! -d "$SRCDIR/dojo" ]; then
+    echo "Can not find dojo resources in 'src/dojo'..."
+    git clone https://github.com/dojo/dojo "$SRCDIR/dojo"
+fi
+
+if [ ! -d "$SRCDIR/dijit" ]; then
+    echo "Can not find dijit resources in 'src/dijit'..."
+    git clone https://github.com/dojo/dijit "$SRCDIR/dijit"
+fi
+
+if [ ! -d "$SRCDIR/dojox" ]; then
+    echo "Can not find dojox resources in 'src/dojox'..."
+    git clone https://github.com/dojo/dojox "$SRCDIR/dojox"
+fi
+
+if [ ! -d "$SRCDIR/util" ]; then
+    echo "Can not find util resources in 'src/util'..."
+    git clone https://github.com/dojo/util "$SRCDIR/util"
+fi
+
 coffee -cw --output $BASEDIR/../src $BASEDIR/../coffee
