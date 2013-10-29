b7bc9f00b6733c8b64b92a99f37f76ab94d8fcba
h-iwata <h.iwata@astamuse.co.jp>
2013-10-29 15:38:56 +0900

    set debug mode to config.js when start coffee watch

--- javascripts/tools/startCoffeeWatch.sh
+++ javascripts/tools/startCoffeeWatch.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 BASEDIR=$(cd $(dirname $0) && pwd)
+echo 'set debug mode to config.js'
+perl -pwi -e "s/var debug = false;/var debug = true;/g" "$BASEDIR/../config.js"
+echo 'done!'
 coffee -cw --output $BASEDIR/../src $BASEDIR/../coffee
