d1ac624417b538288e5d228a0df00e99e1390bfb
h-iwata <h.iwata@astamuse.com>
2014-04-02 15:12:42 +0900

    fix build script

--- javascripts/tools/build/startBuild.sh
+++ javascripts/tools/build/startBuild.sh
@@ -23,32 +23,13 @@ LOADERCONF="$BASEDIR/config.js"
 # Directory containing dojo build utilities
 TOOLSDIR="$SRCDIR/util/buildscripts"
 
-if [ ! -d "$SRCDIR/dojo" ]; then
-    echo "Can not find dojo resources in 'src/dojo'..."
-    git clone https://github.com/dojo/dojo "$SRCDIR/dojo"
-fi
-
-if [ ! -d "$SRCDIR/dijit" ]; then
-    echo "Can not find dijit resources in 'src/dijit'..."
-    git clone https://github.com/dojo/dijit "$SRCDIR/dijit"
-fi
-
-if [ ! -d "$SRCDIR/dojox" ]; then
-    echo "Can not find dojox resources in 'src/dojox'..."
-    git clone https://github.com/dojo/dojox "$SRCDIR/dojox"
-fi
-
-if [ ! -d "$SRCDIR/util" ]; then
-    echo "Can not find util resources in 'src/util'..."
-    git clone https://github.com/dojo/util "$SRCDIR/util"
-fi
-
 if [ ! -d "$DISTDIR" ]; then
 	mkdir $DISTDIR
 fi
 
 echo "compile coffee to javascript..."
-#coffee -c --output $SRCDIR $COFFEEDIR
+  $BASEDIR/../prepareCoffee.sh
+  coffee -c --output $SRCDIR $COFFEEDIR
 echo "done!"
 
 if [ -d "$DISTDIR" ]; then
--- javascripts/tools/startCoffeeWatch.sh
+++ javascripts/tools/startCoffeeWatch.sh
@@ -5,36 +5,6 @@ BASEDIR=$(cd $(dirname $0) && pwd)
 # Source directory for unbuilt code
 SRCDIR="$BASEDIR/../src"
 
-echo 'set debug mode to config.js'
-perl -pwi'.bak' -e "s/var debug = false;/var debug = true;/g" "$BASEDIR/../config.js"
-echo 'done!'
-
-for dirlist in `ls -l $BASEDIR/../coffee | awk '$1 ~ /d/ {print $9 }'` 
-do
-  if [ -f "$BASEDIR/../coffee/$dirlist/package.json" ]; then
-    echo "Copy $dirlist/package.json to src folder"
-    cp $BASEDIR/../coffee/$dirlist/package.json $SRCDIR/$dirlist/package.json
-  fi
-done
-
-if [ ! -d "$SRCDIR/dojo" ]; then
-    echo "Can not find dojo resources in 'src/dojo'..."
-    git clone https://github.com/dojo/dojo "$SRCDIR/dojo"
-fi
-
-if [ ! -d "$SRCDIR/dijit" ]; then
-    echo "Can not find dijit resources in 'src/dijit'..."
-    git clone https://github.com/dojo/dijit "$SRCDIR/dijit"
-fi
-
-if [ ! -d "$SRCDIR/dojox" ]; then
-    echo "Can not find dojox resources in 'src/dojox'..."
-    git clone https://github.com/dojo/dojox "$SRCDIR/dojox"
-fi
-
-if [ ! -d "$SRCDIR/util" ]; then
-    echo "Can not find util resources in 'src/util'..."
-    git clone https://github.com/dojo/util "$SRCDIR/util"
-fi
+$BASEDIR/prepareCoffee.sh
 
 coffee -cw --output $BASEDIR/../src $BASEDIR/../coffee
